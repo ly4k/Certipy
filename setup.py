3e978d1909268c708ee7d1695ed5a4320143b54d
Oliver Lyak <53348818+ly4k@users.noreply.github.com>
2022-02-25 00:59:48 +0100

    2.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Certipy",
-    version="2.0.7",
+    version="2.0.8",
     license="MIT",
     author="ly4k",
     url="https://github.com/ly4k/Certipy",
