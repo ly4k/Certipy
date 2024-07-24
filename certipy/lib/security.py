118d9263afe80ffae30afaa03499cd3301f513cd
Bryan De Houwer <bryandehouwer@hotmail.com>
2024-07-24 08:27:12 +0200

    Fix CASecurity rights parsing

--- certipy/lib/security.py
+++ certipy/lib/security.py
@@ -42,27 +42,50 @@ class ActiveDirectorySecurity:
             mask = self.RIGHTS_TYPE(ace["Ace"]["Mask"]["Mask"])
             if ace["AceType"] == ldaptypes.ACCESS_ALLOWED_ACE.ACE_TYPE:
                 self.aces[sid]["rights"] |= mask
+
                 if self.RIGHTS_TYPE.EXTENDED_RIGHT & mask:
                     self.aces[sid]["extended_rights"].append(EXTENDED_RIGHTS_NAME_MAP["All-Extended-Rights"])
 
-            if ace["AceType"] == ldaptypes.ACCESS_ALLOWED_OBJECT_ACE.ACE_TYPE:
-                if self.RIGHTS_TYPE.EXTENDED_RIGHT & mask:
-                    if ace["Ace"]["Flags"] == 2:
-                        uuid = bin_to_string(ace["Ace"]["InheritedObjectType"]).lower()
-                    elif ace["Ace"]["Flags"] == 1:
-                        uuid = bin_to_string(ace["Ace"]["ObjectType"]).lower()
-                    else:
-                        continue
+            if ace["AceType"] == ldaptypes.ACCESS_ALLOWED_OBJECT_ACE.ACE_TYPE and \
+               self.RIGHTS_TYPE.EXTENDED_RIGHT & mask and \
+               ace['Ace'].hasFlag(ldaptypes.ACCESS_ALLOWED_OBJECT_ACE.ACE_OBJECT_TYPE_PRESENT):
 
-                    self.aces[sid]["extended_rights"].append(uuid)
-
-
-class CASecurity(ActiveDirectorySecurity):
-    RIGHTS_TYPE = CERTIFICATION_AUTHORITY_RIGHTS
+                uuid = bin_to_string(ace["Ace"]["ObjectType"]).lower()
+                self.aces[sid]["extended_rights"].append(uuid)
 
 
 class CertifcateSecurity(ActiveDirectorySecurity):
     RIGHTS_TYPE = CERTIFICATE_RIGHTS
+
+
+class CASecurity:
+    RIGHTS_TYPE = CERTIFICATION_AUTHORITY_RIGHTS
+
+    def __init__(
+        self,
+        security_descriptor: bytes,
+    ):
+        sd = ldaptypes.SR_SECURITY_DESCRIPTOR()
+        sd.fromString(security_descriptor)
+        self.sd = sd
+
+        self.owner = format_sid(sd["OwnerSid"].getData())
+        self.aces = {}
+
+        aces = sd["Dacl"]["Data"]
+        for ace in aces:
+            sid = format_sid(ace["Ace"]["Sid"].getData())
+
+            if sid not in self.aces:
+                self.aces[sid] = {
+                    "rights": self.RIGHTS_TYPE(0),
+                    "extended_rights": [],
+                    "inherited": ace["AceFlags"] & INHERITED_ACE == INHERITED_ACE,
+                }
+
+            mask = self.RIGHTS_TYPE(ace["Ace"]["Mask"]["Mask"])
+            if ace["AceType"] == ldaptypes.ACCESS_ALLOWED_ACE.ACE_TYPE:
+                self.aces[sid]["rights"] |= mask
 
 
 def is_admin_sid(sid: str):
