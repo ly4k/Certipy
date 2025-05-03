2bf85b3aa01ee4b9fb9de2555deb6f9d3839b42a
Oliver Lyak <53348818+ly4k@users.noreply.github.com>
2025-05-03 15:46:29 +0200

    Update version.py to modern alternatives
    Fixes #215

--- certipy/version.py
+++ certipy/version.py
@@ -1,12 +1,24 @@
-import pkg_resources
-from impacket import __path__
+# Initialize version as unknown
+version = "?"
 
+# Try modern importlib.metadata approach (Python 3.8+)
 try:
-    version = pkg_resources.get_distribution("certipy-ad").version
-except pkg_resources.DistributionNotFound:
-    version = "?"
-    print(
-        "Cannot determine Certipy version. "
-        'If running from source you should at least run "python setup.py egg_info"'
-    )
+    from importlib.metadata import version as get_version
+    version = get_version("certipy-ad")
+except ImportError:
+    # For Python < 3.8, try importlib_metadata backport
+    try:
+        from importlib_metadata import version as get_version
+        version = get_version("certipy-ad")
+    except ImportError:
+        # Fall back to pkg_resources (setuptools)
+        try:
+            import pkg_resources
+            version = pkg_resources.get_distribution("certipy-ad").version
+        except (ImportError, pkg_resources.DistributionNotFound):
+            print(
+                "Cannot determine Certipy version. "
+                'If running from source you should at least run "python setup.py egg_info"'
+            )
+
 BANNER = "Certipy v{} - by Oliver Lyak (ly4k)\n".format(version)
