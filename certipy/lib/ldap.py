d7f10598b41879c39638e7dcc5b4da1dd70edbdc
Oliver Lyak <53348818+ly4k@users.noreply.github.com>
2025-05-07 00:19:01 +0200

    Removed print statement

--- certipy/lib/ldap.py
+++ certipy/lib/ldap.py
@@ -674,8 +674,6 @@ class LDAPConnection:
             sids.add(f"{self.domain_sid}-513")  # Domain Users
             sids.add(f"{self.domain_sid}-515")  # Domain Computers
 
-        print("SIDs:", sids)
-
         # Collect DNs to search for group membership
         dns = [user.get("distinguishedName")]
         for sid in sids:
