8884ca8559b44b018984d1a4d745b5a9f0bcef46
Signum21 <Signum21@users.noreply.github.com>
2025-05-16 21:31:42 +0200

    Missing OID Remote Desktop Authentication (#277)

--- certipy/lib/constants.py
+++ certipy/lib/constants.py
@@ -468,6 +468,7 @@ OID_TO_STR_MAP = {
     # Standard certificate usages
     "1.3.6.1.5.5.7.3.1": "Server Authentication",
     "1.3.6.1.5.5.7.3.2": "Client Authentication",
+    "1.3.6.1.4.1.311.54.1.2": "Remote Desktop Authentication",
     "1.3.6.1.5.5.7.3.3": "Code Signing",
     "1.3.6.1.5.5.7.3.4": "Secure Email",
     "1.3.6.1.5.5.7.3.5": "IP security end system",
