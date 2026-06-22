e35f5099b118a1aa08f5d7dbd329bede3c87049a
Alexander Neff <alex99.neff@gmx.de>
2026-06-22 10:12:59 -0400

    Remove leftover pprint

--- certipy/commands/find.py
+++ certipy/commands/find.py
@@ -1982,10 +1982,6 @@ class Find:
                     )
 
                 # ESC17: Server authentication with enrollee-supplied subject
-                if template.get("enrollee_supplies_subject"):
-                    from pprint import pprint
-
-                    pprint(template["attributes"])
                 if template.get("enrollee_supplies_subject") and template.get(
                     "server_authentication"
                 ):
